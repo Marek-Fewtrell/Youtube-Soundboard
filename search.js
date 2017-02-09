94308392d8b6b108ab162b97e7599ca68918667d
Marek Fewtrell <Marek_Fewtrell@outlook.com.au>
2017-02-09 21:53:12 +1000

    Added objects to search.js. Using objects to make the code better hopefully.

--- search.js
+++ search.js
@@ -1,10 +1,44 @@
 
 
-var maxResultsDisplay = 5; //Number of results to display per page.
-var searchCollection = []; //collection of search pages for current search query.
-var searchNextPageToken = ""; //search next page token
-var searchCurrentPage = 0; //Search current page of current search query
-var searchTotalPages = 0; //Total number of pages for current search query
+var Search = function(){
+	var searchCollection = []; //collection of search pages for current search query.
+	
+	this.addItem = function(item) {
+		searchCollection.push(item);
+	};
+	
+	this.getCollection = function() {
+		return searchCollection;
+	};
+	this.collectionLength = function() {
+		return searchCollection.length;
+	};
+	
+	this.maxResultsDisplay = 5; //Number of results to display per page.
+	this.searchNextPageToken = ""; //search next page token
+	this.searchCurrentPage = 0; //Search current page of current search query
+	this.searchTotalPages = 0; //Total number of pages for current search query
+};
+
+var SearchItem = function( index, videoId, title, channelId, channelTitle, thumbnailUrl, thumbnailsHeight, thumbnailsWidth) {
+	this.id = index;
+	this.videoId = videoId;
+	this.videoTitle = title;
+	this.channelId = channelId;
+	this.channelTitle = channelTitle;
+	this.thumbnail = {};
+	this.thumbnail.thumbnailUrl = thumbnailUrl;
+	this.thumbnail.thumbnailHeight = thumbnailsHeight;
+	this.thumbnail.thumbnailWidth = thumbnailsWidth;
+};
+
+var mySearch = new Search();
+
+//var maxResultsDisplay = 5; //Number of results to display per page.
+//var searchCollection = []; //collection of search pages for current search query.
+//var searchNextPageToken = ""; //search next page token
+//var searchCurrentPage = 0; //Search current page of current search query
+//var searchTotalPages = 0; //Total number of pages for current search query
 
 /*
  * Function: search
@@ -47,31 +81,21 @@ function search(pageToken) {
 		
 		$.each(results.items, function(index, item) {
 			
-			var searchItem = {
-				"id" : index,
-				"videoId" : item.id.videoId,
-				"videoTitle" : item.snippet.title,
-				"channelId" : item.snippet.channelId,
-				"channelTitle" : item.snippet.channelTitle,
-				"thumbnail" : {
-					"thumbnailUrl" : item.snippet.thumbnails.default.url,
-					"thumbnailHeight" : item.snippet.thumbnails.default.height,
-					"thumbnailWidth" : item.snippet.thumbnails.default.width
-				}
-			};
+			var newSearchItem = new SearchItem(index, item.id.videoId, item.snippet.title, item.snippet.channelId, item.snippet.channelTitle, item.snippet.thumbnails.default.url, item.snippet.thumbnails.default.height, item.snippet.thumbnails.default.width);
 			
-			searchCollection.push(searchItem);
-			searchPopulateItem(searchItem);
+			//searchCollection.push(newSearchItem);
+			mySearch.addItem(newSearchItem);
+			searchPopulateItem(newSearchItem);
 		});
 		
-		var totalPages = searchCollection.length / maxResultsDisplay;
-    if ((searchCollection.length % maxResultsDisplay) > 0 ) {
+		var totalPages = mySearch.collectionLength() / mySearch.maxResultsDisplay;
+    if ((mySearch.collectionLength() % mySearch.maxResultsDisplay) > 0 ) {
     	totalPages++;
     }
     
-    searchTotalPages = totalPages;
+    mySearch.searchTotalPages = totalPages;
     
-    searchNextPageToken = (response.result.nextPageToken != undefined) ? response.result.nextPageToken : "";
+    mySearch.searchNextPageToken = (response.result.nextPageToken != undefined) ? response.result.nextPageToken : "";
     
     if (response.result.nextPageToken != undefined) {
     	$("#nextSearchPageBtn").prop("disabled", false);
@@ -127,34 +151,35 @@ function searchPopulateItem(item) {
 function handleSearchPages(action) {
 	
 	if (action == "next" ) {
-		searchCurrentPage++;
+		mySearch.searchCurrentPage++;
 	} else if (action == "previous") {
-		searchCurrentPage--;
+		mySearch.searchCurrentPage--;
 	}
 
-	var startIndex = searchCurrentPage * maxResultsDisplay;
-	var endIndex = ((startIndex+maxResultsDisplay) > searchCollection.length) ? searchCollection.length : startIndex+maxResultsDisplay;
+	var startIndex = mySearch.searchCurrentPage * mySearch.maxResultsDisplay;
+	var endIndex = ((startIndex+mySearch.maxResultsDisplay) > mySearch.collectionLength()) ? mySearch.collectionLength() : startIndex+mySearch.maxResultsDisplay;
 	
 
-	if (searchNextPageToken == "" && searchCurrentPage + 1 > searchTotalPages-1) {
+	if (mySearch.searchNextPageToken == "" && mySearch.searchCurrentPage + 1 > mySearch.searchTotalPages-1) {
 		$("#nextSearchPageBtn").prop("disabled", true);
 	} else {
 		$("#nextSearchPageBtn").prop("disabled", false);
 	}
 
-	if (searchCurrentPage - 1 < 0) {
+	if (mySearch.searchCurrentPage - 1 < 0) {
 		$("#previosSearchPageBtn").prop("disabled", true);
 	} else {
 		$("#previosSearchPageBtn").prop("disabled", false);
 	}
 
 	$("#search-container").empty();
+	var tempCollection = mySearch.getCollection();
 	for (var i = startIndex; i < endIndex; i++) {
-		searchPopulateItem(searchCollection[i]);
+		searchPopulateItem(tempCollection[i]);
 	}
 	
-	if (searchNextPageToken != "" && startIndex >= searchCollection.length) {
-		search(searchNextPageToken);
+	if (mySearch.searchNextPageToken != "" && startIndex >= mySearch.collectionLength()) {
+		search(mySearch.searchNextPageToken);
 		$("#previosSearchPageBtn").prop("disabled", false);
 		return;
 	}
